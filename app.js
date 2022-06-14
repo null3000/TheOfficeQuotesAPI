3ac3848bad17075f3faa065f311c3556f0f11b31
Null3000 <vaughnbosu@vaughns-mbp.lan>
2022-06-13 17:40:30 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const PORT = 8080;
+
+
+
+app.get("/ping", (req, res, next) => {
+    res.json([{"success" : true}]);
+    res.status(200)
+});
+
+app.get('/quote/dwight', (req, res) => {
+    let quotes = require('./dwightquotes.json');
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(200).send({
+        data:quote
+    })
+});
+app.get('/quote/micheal', (req, res) => {
+    let quotes = require('./michealquotes.json');
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(200).send({
+        data:quote
+    })
+});
+
+app.listen(
+    PORT,
+    () => console.log(`Server is running on port http://localhost:${PORT}`)
+)
