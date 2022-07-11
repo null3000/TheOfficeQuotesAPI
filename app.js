ca01cc59c924428ad27ad96fd2f337d24b1f8394
Vaughn Bosu <76852813+null3000@users.noreply.github.com>
2022-07-10 18:11:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const app = require('express')();
 const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+
+app.use(cors());
 
 app.get("/ping", (req, res, next) => {
     res.json([{"success" : true}]);
     res.status(200)
 });
 
-app.get('/quote/dwight', (req, res) => {
+app.get('/api/v1/quote/dwight', (req, res) => {
     let quotes = require('./dwightquotes.json');
     let quote = quotes[Math.floor(Math.random() * quotes.length)];
     res.status(200).send(quote)
 });
 
-app.get('/quote/micheal', (req, res) => {
+app.get('/api/v1/quote/micheal', (req, res) => {
     let quotes = require('./michealquotes.json');
     let quote = quotes[Math.floor(Math.random() * quotes.length)];
     res.status(200).send(quote)
